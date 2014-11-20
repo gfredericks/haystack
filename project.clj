c16da4601716ef4e16a99f4e02bb081d5ddad99e
Gary Fredericks <fredericksgary@gmail.com>
2014-11-20 10:40:17 -0600

    Switch to lein's release setup

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject com.gfredericks/haystack "0.2.0"
+(defproject com.gfredericks/haystack "0.2.1-SNAPSHOT"
   :description "Dataset Difference Debugging in Clojure"
   :url "https://github.com/fredericksgary/haystack"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :deploy-repositories [["releases" :clojars]])
